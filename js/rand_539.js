2ddc514e95576fda3d10f3a90219eed5d70b9c3e
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-02-25 17:55:33 +0800

    Update rand_539.js

--- js/rand_539.js
+++ js/rand_539.js
@@ -3,16 +3,15 @@ function rand(max) {
 }
 
 function roll() {
-let num = 39;
-let arr = [];
-let random = rand(num);
+	let num = 39;
+	let arr = [];
+	let random = rand(num);
 
-for (i = 1; i <= 5; i++) {
-                arr.push(random);
-                while (arr.includes(random)) {
-                    random = rand(num);
-                }	
-            }
-			
+	for (i = 1; i <= 5; i++) {
+		arr.push(random);
+		while (arr.includes(random)) {
+			random = rand(num);
+		}
+	}
 document.getElementById("lotto539").innerHTML='開獎號碼:'+"&nbsp;"+arr.sort(function(a, b){return a-b})
 }
